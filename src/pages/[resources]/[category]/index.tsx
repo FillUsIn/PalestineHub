a7cd248ac000686aca178cfbea35bb0eb53ee5ce
Ahmad Alghali <ahmad-mustafaalghali@hotmail.com>
2023-10-25 02:16:17 +0100

    display selected category

--- src/pages/[resources]/[category]/index.tsx
+++ src/pages/[resources]/[category]/index.tsx
@@ -1,9 +1,13 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 type Props = {};
 
 function CategoryPage({}: Props) {
-  return <div>CategoryPage</div>;
+  const router = useRouter();
+
+  const { category } = router.query;
+  return <div>{category}</div>;
 }
 
 export default CategoryPage;
