1c2ec7055b5e89fb2ef7267735135295375dafc8
Zaidh Imran <zaidhimran2000@gmail.com>
2023-10-26 22:29:24 -0230

    updated post cards

--- src/components/ResourcePost.tsx
+++ src/components/ResourcePost.tsx
@@ -3,31 +3,32 @@ import { Paper, Text, Group, Title, Button } from '@mantine/core';
 import UpvoteDownvote from './UpvoteDownvote';
 import { PostSummaryDTO } from '@/types/dtos';
 
-interface Props {
-  postSummary: PostSummaryDTO;
-}
+type Props = {
+  post: PostSummaryDTO;
+};
 
-function ResourcePost({ postSummary }: Props) {
+function ResourcePost({ post }: Props) {
   return (
     <Paper
       shadow='md'
       radius='lg'
+      style={{ backgroundImage: `url(${post.thumbnailUrl})` }}
       className='flex h-40 w-full flex-col justify-between overflow-hidden md:h-80'
     >
       <div className='flex h-full flex-col justify-between bg-gradient-to-b from-[#141414eb] to-90% p-5'>
         <div>
           <Text size='sm' c='gray' className='text-white-400 capitalize'>
-            {postSummary.username}
+            {post.username}
           </Text>
           <div className='mt-2'>
             <Title order={1} className='text-white-700 uppercase'>
-              {postSummary.title}
+              {post.title}
             </Title>
             <Text size='xs' className='capitalize'>
               description/link
             </Text>
           </div>
-          <p className='mt-3 font-semibold text-white'>{postSummary.body}</p>
+          <p className='mt-3 font-semibold text-white'>{post.body}</p>
         </div>
         <div className='items-right flex justify-between'>
           <span>{/**wrapper*/}</span>
--- src/components/TopVotedPost.tsx
+++ src/components/TopVotedPost.tsx
@@ -3,32 +3,38 @@ import { Paper, Text, Group, Title, Button } from '@mantine/core';
 import UpvoteDownvote from './UpvoteDownvote';
 import { PostSummaryDTO } from '@/types/dtos';
 
-interface Props {
-  postSummary: PostSummaryDTO;
-}
+type Props = {
+  post: PostSummaryDTO;
+};
 
-function TopVotedPost({ postSummary }: Props) {
+function TopVotedPost({ post }: Props) {
   return (
     <Paper
       shadow='md'
       radius='lg'
+      style={{ backgroundImage: `url(${post.thumbnailUrl})` }}
       className='flex h-40 w-full flex-col justify-between overflow-hidden md:h-80'
     >
       <div className='flex h-full flex-col justify-between bg-gradient-to-b from-[#141414eb] to-90% p-5'>
         <div>
           <div>
             <Title order={1} className='text-white-700 uppercase'>
-              {postSummary.title}
+              {post.title}
             </Title>
             <Text size='xs' className='capitalize'>
               description/link
             </Text>
           </div>
-          <p className='mt-3 font-semibold text-white'>{postSummary.body}</p>
+          <p className='mt-3 font-semibold text-white'>{post.body}</p>
         </div>
         <div className='flex items-center justify-between'>
           <span className='mr-6'>
-            <Button variant='outline' color='dark' size='regular-md'>
+            <Button
+              variant='white'
+              color='black'
+              size='regular-md'
+              style={{ border: '1px solid black' }}
+            >
               Read Post
             </Button>
           </span>
