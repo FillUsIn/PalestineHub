e6945e4d8a5064d0fd9928009fbce594cfa9fd02
Zaidh Imran <zaidhimran2000@gmail.com>
2023-10-26 21:40:11 -0230

    Resource post made

---
+++ src/components/ResourcePost.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Paper, Text, Group, Title, Button } from '@mantine/core';
+import UpvoteDownvote from './UpvoteDownvote';
+import { PostSummaryDTO } from '@/types/dtos';
+
+interface Props {
+  postSummary: PostSummaryDTO;
+}
+
+function ResourcePost({ postSummary }: Props) {
+  return (
+    <Paper
+      shadow='md'
+      radius='lg'
+      className='flex h-40 w-full flex-col justify-between overflow-hidden md:h-80'
+    >
+      <div className='flex h-full flex-col justify-between bg-gradient-to-b from-[#141414eb] to-90% p-5'>
+        <div>
+          <Text size='sm' c='gray' className='text-white-400 capitalize'>
+            {postSummary.username}
+          </Text>
+          <div className='mt-2'>
+            <Title order={1} className='text-white-700 uppercase'>
+              {postSummary.title}
+            </Title>
+            <Text size='xs' className='capitalize'>
+              description/link
+            </Text>
+          </div>
+          <p className='mt-3 font-semibold text-white'>{postSummary.body}</p>
+        </div>
+        <div className='items-right flex justify-between'>
+          <span>{/**wrapper*/}</span>
+          <span>
+            <UpvoteDownvote></UpvoteDownvote>
+          </span>
+        </div>
+      </div>
+    </Paper>
+  );
+}
+
+export default ResourcePost;
