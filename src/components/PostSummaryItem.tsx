84bbf3839cad1f311668ad36be830bcba244ba0a
Awaes Ishfaq <awaesishfaq@gmail.com>
2023-10-28 21:04:59 +0100

    Remove Link

--- src/components/PostSummaryItem.tsx
+++ src/components/PostSummaryItem.tsx
@@ -14,9 +14,11 @@ type Props = {
 function PostSummaryItem({ post }: Props) {
   const {
     body,
+    categoryName,
     commentCount,
     createdDate,
     id,
+    subCategoryName,
     title,
     thumbnailUrl,
     username,
@@ -24,47 +26,47 @@ function PostSummaryItem({ post }: Props) {
   } = post;
   const formattedDate = since(createdDate);
   return (
-    <Link href={`/posts/${id}`}>
-      <Paper
-        className='h-48 overflow-hidden text-black'
-        radius='lg'
-        withBorder={true}
-      >
-        <div className='flex flex-row justify-between'>
-          {thumbnailUrl && (
-            <div className='w-1/3'>
-              <img alt={title} className='w-full' src={thumbnailUrl} />
-            </div>
-          )}
+    <Paper
+      className='flex flex-col h-48 overflow-hidden text-black transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 hover: duration-300'
+      component={Link}
+      href={`/resources/${categoryName}/${subCategoryName}/posts/${id}`}
+      radius='lg'
+      withBorder={true}
+    >
+      <div className='flex flex-row justify-between'>
+        {thumbnailUrl && (
+          <div className='w-1/3'>
+            <img alt={title} className='w-full' src={thumbnailUrl} />
+          </div>
+        )}
 
-          <div className='basis-2/3 flex h-full flex-col justify-between bg-[white] p-5'>
-            <div>
-              <div className='flex items-center'>
-                <p className='text-sm font-medium'>
-                  {username}
-                  <span className='text-xs font-semibold '>
-                    <span className='mx-1'>·</span>
-                    <span>{formattedDate}</span>
-                  </span>
-                </p>
-              </div>
-
-              <h3 className='mt-2 text-xl font-semibold'>{title}</h3>
-              <p className='mb-2 mt-8 whitespace-pre-line break-words text-sm font-medium'>
-                {truncate(body, 200)}
+        <div className='basis-2/3 flex h-full flex-col justify-between bg-[white] p-5'>
+          <div>
+            <div className='flex items-center'>
+              <p className='text-sm font-medium'>
+                {username}
+                <span className='text-xs font-semibold '>
+                  <span className='mx-1'>·</span>
+                  <span>{formattedDate}</span>
+                </span>
               </p>
-
-              <ContentInteractions
-                voteCount={voteCount}
-                commentCount={commentCount}
-                showOptions={false}
-                onOptionsClicked={() => {}}
-              />
             </div>
+
+            <h3 className='mt-2 text-xl font-semibold'>{title}</h3>
+            <p className='mb-2 mt-8 whitespace-pre-line break-words text-sm font-medium'>
+              {truncate(body, 200)}
+            </p>
+
+            <ContentInteractions
+              voteCount={voteCount}
+              commentCount={commentCount}
+              showOptions={false}
+              onOptionsClicked={() => {}}
+            />
           </div>
         </div>
-      </Paper>
-    </Link>
+      </div>
+    </Paper>
   );
 }
 
