3dca44c4e446376f3367f9b457065bc056826699
Ahmad Alghali <60392502+ahmadalghali@users.noreply.github.com>
2023-10-29 13:47:02 +0000

    fix layout issue (#38)

--- src/pages/resources/index.tsx
+++ src/pages/resources/index.tsx
@@ -37,8 +37,8 @@ function Resources({ paginatedPosts }: Props) {
 
   return (
     <>
-      <div className='flex'>
-        <div className='mr-8'>
+      <div className='flex justify-center'>
+        <div className='pr-5'>
           <NavbarNested />
         </div>
         <div className='flex flex-col'>
